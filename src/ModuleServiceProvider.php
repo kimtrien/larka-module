292b0bc873f590cd11bae8b376f3003661d5329f
kjmtrue <kimtrien@gmail.com>
2016-10-11 18:47:22 +0700

    Settup

---
+++ src/ModuleServiceProvider.php
@@ -0,0 +1,51 @@
+<?php
+namespace Kjmtrue\Module;
+
+use Illuminate\Support\ServiceProvider;
+
+class ModuleServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->publishes([
+            __DIR__ . '/../config/modules.php' => config_path('modules.php'),
+        ]);
+
+        // Load list modules
+        $modules = config('module.modules');
+
+        foreach ($modules as $module) {
+            $module_dir = base_path("modules/{$module}");
+
+            // Load routes.php module
+            if(file_exists($module_dir . '/routes.php') & !$this->app->routesAreCached()){
+                require $module_dir . '/routes.php';
+            }
+
+            // Load view modules
+            if(is_dir($module_dir . '/resources/views')){
+                $this->loadViewsFrom($module_dir . '/resources/views', $module);
+            }
+
+            // Load lang modules
+            if(is_dir($module_dir . '/resources/lang')){
+                $this->loadTranslationsFrom($module_dir . '/resources/lang', $module);
+            }
+        }
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
